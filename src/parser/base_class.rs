cd88691fc821f40ce7b1164f82957defd1795c5a
浦添阳 <putianyang@conchship.net>
2023-06-05 15:23:27 +0800

    base_class

--- src/parser/base_class.rs
+++ src/parser/base_class.rs
@@ -29,7 +29,7 @@ impl CodeGenerator for BaseClass {
     type Output = String;
 
     fn gen_code(&self, end: &'static str, tab_nums: i32) -> Self::Output {
-        let mut code = String::with_capacity(512);
+        let mut code = String::with_capacity(1024);
         
         let format = |n: i32, code: &mut String| {
             for _ in 0..n {
@@ -126,7 +126,7 @@ impl CodeGenerator for BaseClass {
                         code.push_str(end);
                     }
 
-                    //--------------------------Init----------------------------------
+                    //--------------------------Init-begin----------------------------------
                     code.push_str(end);
                     format(tab_nums + 1, &mut code);
                     code.push_str("public void Init()");
@@ -162,9 +162,9 @@ impl CodeGenerator for BaseClass {
                     format(tab_nums + 1, &mut code);
                     code.push('}');
                     code.push_str(end);
-                    //--------------------------Init----------------------------------
+                    //--------------------------Init-end----------------------------------
 
-                    //--------------------------GetItemId----------------------------------
+                    //--------------------------GetItemId-begin----------------------------------
                     code.push_str(end);
                     format(tab_nums + 1, &mut code);
                     code.push_str("public int GetItemId(string refName)");
@@ -184,7 +184,7 @@ impl CodeGenerator for BaseClass {
                     format(tab_nums + 1, &mut code);
                     code.push('}');
                     code.push_str(end);
-                    //--------------------------GetItemId----------------------------------
+                    //--------------------------GetItemId-end----------------------------------
 
                     code.push_str(end);
                     format(tab_nums + 1, &mut code);
@@ -197,10 +197,277 @@ impl CodeGenerator for BaseClass {
                     // empty line
                     code.push_str(end);
 
-                    //--------------------------AddExtraItem----------------------------------
-                    
-                    //--------------------------AddExtraItem----------------------------------
+                    //--------------------------AddExtraItem-begin----------------------------------
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public int AddExtraItem(string identifier, string refName, object configItem)");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("var item = (");
+                    code.push_str(&self.name);
+                    code.push_str("Item)configItem;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("var id = (int) item.TemplateId;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (id < _dataArray.Count)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("throw new Exception($\"");
+                    code.push_str(&self.name);
+                    code.push_str(" template id {item.TemplateId} created by {identifier} already exist.\");");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (_extraDataMap.ContainsKey(id))");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("throw new Exception($\"");
+                    code.push_str(&self.name);
+                    code.push_str(" extra template id {item.TemplateId} created by {identifier} already exist.\");");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (_refNameMap.TryGetValue(refName, out var refId))");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("throw new Exception($\"");
+                    code.push_str(&self.name);
+                    code.push_str(" template reference name {refName}(id = {item.TemplateId}) created by {identifier} already exist with templateId {refId}).\");");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("_refNameMap.Add(refName, id);");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("_extraDataMap.Add(id, item);");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("return id;");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    //--------------------------AddExtraItem-end----------------------------------
 
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public ");
+                    code.push_str(&self.name);
+                    code.push_str("Item this[sbyte id] => GetItem(id);");
+                    code.push_str(end);
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public ");
+                    code.push_str(&self.name);
+                    code.push_str("Item this[int id] => GetItem((sbyte)id);");
+                    code.push_str(end);
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public ");
+                    code.push_str(&self.name);
+                    code.push_str("Item this[string refName] => this[_refNameMap[refName]];");
+                    code.push_str(end);
+                    code.push_str(end);
+
+                    //--------------------------GetItem-begin----------------------------------
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public ");
+                    code.push_str(&self.name);
+                    code.push_str("Item GetItem(sbyte id)");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (id < 0) return null;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (id < _dataArray.Count) return _dataArray[(int)id];");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if (_extraDataMap.TryGetValue((int) id, out var item)) return item;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("// 预期为有效 Id 但仍然访问不到数据时");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("GameData.Utilities.AdaptableLog.TagWarning(GetType().FullName, $\"index {id} is not in range [0, {_dataArray.Count}) and is not defined in _extraDataMap (count: {_extraDataMap.Count})\");");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("return null;");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    code.push_str(end);
+                    //--------------------------GetItem-end----------------------------------
+                    
+                    //--------------------------RequiredFields-begin----------------------------------
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("private readonly HashSet<string> RequiredFields = new HashSet<string>()");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    for v in requires.iter() {
+                        if let Some(vv) = v {
+                            format(tab_nums + 2, &mut code);
+                            code.push('\"');
+                            code.push_str(vv);
+                            code.push('\"');
+                            code.push(',');
+                            code.push_str(end);
+                        }
+                    }
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("};");
+                    code.push_str(end);
+                    //--------------------------RequiredFields-end----------------------------------
+
+                    //--------------------------GetAllKeys-begin----------------------------------
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public List<sbyte> GetAllKeys()");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("return (from item in _dataArray where null != item select item.TemplateId).ToList();");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    //--------------------------GetAllKeys-end----------------------------------
+
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public int Count => _dataArray.Count;");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public int CountWithExtra => Count + _extraDataMap.Count;");
+                    code.push_str(end);
+
+                    //--------------------------Iterate-begin----------------------------------
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("public void Iterate(Func<");
+                    code.push_str(&self.name);
+                    code.push_str("Item,bool> iterateFunc)");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("if(null == iterateFunc)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("return;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach(");
+                    code.push_str(&self.name);
+                    code.push_str("Item item in _dataArray)");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("if(null == item)");
+                    code.push_str(end);
+                    format(tab_nums + 4, &mut code);
+                    code.push_str("continue;");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("if(!iterateFunc(item))");
+                    code.push_str(end);
+                    format(tab_nums + 4, &mut code);
+                    code.push_str("break;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push('}');
+
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach(");
+                    code.push_str(&self.name);
+                    code.push_str("Item item in _extraDataMap.Values)");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("if(null == item)");
+                    code.push_str(end);
+                    format(tab_nums + 4, &mut code);
+                    code.push_str("continue;");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("if(!iterateFunc(item))");
+                    code.push_str(end);
+                    format(tab_nums + 4, &mut code);
+                    code.push_str("break;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    //--------------------------Iterate-end----------------------------------
+
+                    //--------------------------GetEnumerator-begin----------------------------------
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("IEnumerator<");
+                    code.push_str(&self.name);
+                    code.push_str("Item> IEnumerable<");
+                    code.push_str(&self.name);
+                    code.push_str("Item>.GetEnumerator()");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach (var item in _dataArray)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("yield return item;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach (var item in _extraDataMap.Values)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("yield return item;");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push_str("IEnumerator IEnumerable.GetEnumerator()");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('{');
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach (var item in _dataArray)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("yield return item;");
+                    code.push_str(end);
+                    format(tab_nums + 2, &mut code);
+                    code.push_str("foreach (var item in _extraDataMap.Values)");
+                    code.push_str(end);
+                    format(tab_nums + 3, &mut code);
+                    code.push_str("yield return item;");
+                    code.push_str(end);
+                    format(tab_nums + 1, &mut code);
+                    code.push('}');
+                    code.push_str(end);
+                    //--------------------------GetEnumerator-end----------------------------------
 
                     format(tab_nums, &mut code);
                     code.push('}');
